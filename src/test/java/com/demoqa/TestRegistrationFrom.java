feb9035a4f0f593af579a1b257dce57353931616
kaliaaaaan <48kalian@gmail.com>
2023-08-02 22:13:57 +0300

    OK
    Signed-off-by: kaliaaaaan <48kalian@gmail.com>

--- src/test/java/com/demoqa/TestRegistrationFrom.java
+++ src/test/java/com/demoqa/TestRegistrationFrom.java
@@ -8,7 +8,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class TestRegistration {
+public class TestRegistrationFrom {
 
     @BeforeAll
     static void beforeAll() {
